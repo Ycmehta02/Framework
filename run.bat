03a967aa3ad0813a4ca50735cd2f8828659b5f4b
Yashvi Mehta <yashvi.mehta@aliansoftware.com>
2025-02-19 09:01:26 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,3 @@
-@echo on
-cd C:\Users\Alian\Desktop\YM_HybridFramework
-.venv\Scripts\activate
+call C:\Users\Alian\Desktop\YM_HybridFramework\.venv\Scripts\activate
+
 pytest -v -s -m "sanity" testCases/  --browser chrome
-pause
